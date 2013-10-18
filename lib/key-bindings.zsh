46b77e82e2d10f49b995a7d87a2f1a7030cfbbfa
wyy <wuyanyi09@gmail.com>
2013-10-18 09:11:51 -0700

    add zsh-history-substring-search.zsh

--- lib/key-bindings.zsh
+++ lib/key-bindings.zsh
@@ -8,8 +8,10 @@ bindkey "^[[5~" up-line-or-history
 bindkey "^[[6~" down-line-or-history
 
 # make search up and down work, so partially type and hit up/down to find relevant stuff
-bindkey '^[[A' up-line-or-search
-bindkey '^[[B' down-line-or-search
+#bindkey '^[[A' up-line-or-search
+#bindkey '^[[B' down-line-or-search
+bindkey '^[[A' history-substring-search-up
+bindkey '^[[B' history-substring-search-down
 
 bindkey "^[[H" beginning-of-line
 bindkey "^[[1~" beginning-of-line
